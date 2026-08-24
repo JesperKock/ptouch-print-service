368851c444cf9c07a7a96c8e14da189df39bf2cf
JesperKock <jekoc@kds.dk>
2026-08-24 15:34:41 +0200

    feat: add info endpoint
    This endpoints will return info about the printer

--- main.go
+++ main.go
@@ -20,11 +20,30 @@ type responsePayload struct {
 var validInputRegex = regexp.MustCompile(`^[a-zA-Z0-9-]+$`)
 
 func main() {
+	http.HandleFunc("/info", infoHandler)
 	http.HandleFunc("/print", printHandler)
 
 	fmt.Println("Starting server on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		fmt.Printf("Server error: %s\n", err)
+	}
+}
+
+func infoHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	cmd := exec.Command("ptouch-print", "--info")
+	if err := cmd.Run(); err != nil {
+		response := responsePayload{
+			Message: "Command execution failed",
+			Error:   err.Error(),
+		}
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(response)
+		return
 	}
 }
 
